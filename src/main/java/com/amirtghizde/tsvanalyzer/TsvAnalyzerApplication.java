3ef3607daf7fc322df2575a25a2d7b2c5baa2bd1
AmirTghizde <a.taghizadehmail@gmail.com>
2024-02-25 17:29:37 +0330

    chore: add base files

---
+++ src/main/java/com/amirtghizde/tsvanalyzer/TsvAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.amirtghizde.tsvanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsvAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsvAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amirtghizde/tsvanalyzer/TsvAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amirtghizde.tsvanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsvAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
